a48351c3e02fe85f3c28c4e5b35cc1b1203b0e67
Rafay Zia <rafayzia10@gmail.com>
2023-06-07 15:32:57 +0500

    Added categories UI

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-type Props = {}
 
-const Logo = (props: Props) => {
+const Logo = () => {
     const router = useRouter();
+
   return (
-    <Image alt='logo' className='hidden md:block cursor-pointer' height={100} width={100} src={'/images/logo.png'} />
+    <Image onClick={()=>router.push('/')} alt='logo' className='hidden md:block cursor-pointer' height={100} width={100} src={'/images/logo.png'} />
   )
 }
 
