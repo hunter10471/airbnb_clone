2d000fc36d8de935322ae5d294740bc530d6623b
Rafay Zia <rafayzia10@gmail.com>
2023-06-12 19:05:06 +0500

    Worked on further steps of rent modal

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -6,6 +6,9 @@ import Heading from '../Heading';
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../Inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
+import CountrySelect from '../Inputs/CountrySelect';
+import dynamic from 'next/dynamic';
+import Counter from '../Inputs/Counter';
 
 enum STEPS {
     CATEGORY = 0,
@@ -18,6 +21,7 @@ enum STEPS {
 
 const RentModal = () => {
 
+    
     const {register, handleSubmit, setValue, watch, formState: {
         errors
     }, reset} = useForm<FieldValues>({
@@ -33,8 +37,14 @@ const RentModal = () => {
             description: ''
         }
     });
-
+    
     const category = watch('category'); 
+    const location = watch('location');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
+
+    const Map = useMemo(() => dynamic(() => import('../Map'),{ssr:false}),[location])
 
     const setCustomValue = (id:string, value:any) => {
         setValue(id, value, {
@@ -56,7 +66,7 @@ const RentModal = () => {
         if(step === STEPS.PRICE){
             return 'Create';
         }
-        return 'next';
+        return 'Next';
     },[step])
 
     const secondaryActionLabel = useMemo(()=>{
@@ -81,9 +91,32 @@ const RentModal = () => {
         </div>
     )
 
+    if(step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading title='Where is your place located ?' subtitle='Help guests find you!' />
+                <CountrySelect onChange={(value) => setCustomValue('location', value)} value={location} />
+                <Map center={location?.latlng} />
+            </div>
+        )
+    }
+
+    if(step === STEPS.INFO) {
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading title='Share some basics about your place' subtitle='What amenities do you have ?' />
+                <Counter title='Guests' subtitle='How many guests do you allow ?' value={guestCount} onChange={(value) => setCustomValue('guestCount', value)} />
+                <hr />
+                <Counter title='Rooms' subtitle='How many rooms do you have ?' value={roomCount} onChange={(value) => setCustomValue('roomCount', value)} />
+                <hr />
+                <Counter title='Bathrooms' subtitle='How many bathrooms do you have ?' value={bathroomCount} onChange={(value) => setCustomValue('bathroomCount', value)} />
+            </div>
+        )
+    }
+
     const rentModal = useRentModal();
   return (
-   <Modal title='Airbnb Your Home' body={bodyContent} isOpen={rentModal.isOpen} onClose={rentModal.onClose} onSubmit={rentModal.onClose} actionLabel={actionLabel} secondaryActionLabel={secondaryActionLabel} secondaryAction={step === STEPS.CATEGORY ? undefined : onBack} />
+   <Modal title='Airbnb Your Home' body={bodyContent} isOpen={rentModal.isOpen} onClose={rentModal.onClose} onSubmit={onNext} actionLabel={actionLabel} secondaryActionLabel={secondaryActionLabel} secondaryAction={step === STEPS.CATEGORY ? undefined : onBack} />
   )
 }
 
