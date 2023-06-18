311a44037138c88648301142c3a34163b188c267
Rafay Zia <rafayzia10@gmail.com>
2023-06-18 11:56:12 +0500

    Added cloudinary image uploading for rent modal

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -1,7 +1,7 @@
 'use client';
-import React, { useState, useMemo } from 'react'
-import Modal from './Modal'
-import useRentModal from '@/app/hooks/useRentModal'
+import React, { useState, useMemo } from 'react';
+import Modal from './Modal';
+import useRentModal from '@/app/hooks/useRentModal';
 import Heading from '../Heading';
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../Inputs/CategoryInput';
@@ -9,6 +9,7 @@ import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../Inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../Inputs/Counter';
+import ImageUpload from '../Inputs/ImageUpload';
 
 enum STEPS {
     CATEGORY = 0,
@@ -16,17 +17,20 @@ enum STEPS {
     INFO = 2,
     IMAGES = 3,
     DESCRIPTION = 4,
-    PRICE = 5
+    PRICE = 5,
 }
 
 const RentModal = () => {
-
-    
-    const {register, handleSubmit, setValue, watch, formState: {
-        errors
-    }, reset} = useForm<FieldValues>({
-        defaultValues:{
-            category:'',
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState: { errors },
+        reset,
+    } = useForm<FieldValues>({
+        defaultValues: {
+            category: '',
             location: null,
             guestCount: 1,
             roomCount: 1,
@@ -34,90 +38,152 @@ const RentModal = () => {
             imgSrc: '',
             price: 1,
             title: '',
-            description: ''
-        }
+            description: '',
+        },
     });
-    
-    const category = watch('category'); 
+
+    const category = watch('category');
     const location = watch('location');
     const guestCount = watch('guestCount');
     const roomCount = watch('roomCount');
     const bathroomCount = watch('bathroomCount');
+    const imageSrc = watch('imageSrc');
 
-    const Map = useMemo(() => dynamic(() => import('../Map'),{ssr:false}),[location])
+    const Map = useMemo(
+        () => dynamic(() => import('../Map'), { ssr: false }),
+        [location]
+    );
 
-    const setCustomValue = (id:string, value:any) => {
+    const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
-            shouldValidate:true,
-            shouldDirty:true,
-            shouldTouch:true
-        })
-    }
+            shouldValidate: true,
+            shouldDirty: true,
+            shouldTouch: true,
+        });
+    };
 
     const [step, setStep] = useState(STEPS.CATEGORY);
     const onBack = () => {
-        setStep(value => value - 1);
-    }
+        setStep((value) => value - 1);
+    };
     const onNext = () => {
-        setStep(value => value + 1);
-    }
+        setStep((value) => value + 1);
+    };
 
-    const actionLabel = useMemo(()=>{
-        if(step === STEPS.PRICE){
+    const actionLabel = useMemo(() => {
+        if (step === STEPS.PRICE) {
             return 'Create';
         }
         return 'Next';
-    },[step])
+    }, [step]);
 
-    const secondaryActionLabel = useMemo(()=>{
-        if(step === STEPS.CATEGORY){
+    const secondaryActionLabel = useMemo(() => {
+        if (step === STEPS.CATEGORY) {
             return undefined;
         }
         return 'Back';
-    },[step])
+    }, [step]);
 
     let bodyContent = (
-        <div className='flex flex-col gap-8'>
-            <Heading title='Which of these best describes your place ?' subtitle='Pick a category' />
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'>
-                {
-                    categories.map(item => {
-                        return <div key={item.label} className='col-span-1'>
-                            <CategoryInput onClick={(category)=>{setCustomValue('category', category)}} selected={category === item.label} label={item.label} icon={item.icon} />
+        <div className="flex flex-col gap-8">
+            <Heading
+                title="Which of these best describes your place ?"
+                subtitle="Pick a category"
+            />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+                {categories.map((item) => {
+                    return (
+                        <div key={item.label} className="col-span-1">
+                            <CategoryInput
+                                onClick={(category) => {
+                                    setCustomValue('category', category);
+                                }}
+                                selected={category === item.label}
+                                label={item.label}
+                                icon={item.icon}
+                            />
                         </div>
-                    })
-                }
+                    );
+                })}
             </div>
         </div>
-    )
+    );
 
-    if(step === STEPS.LOCATION) {
+    if (step === STEPS.LOCATION) {
         bodyContent = (
-            <div className='flex flex-col gap-8'>
-                <Heading title='Where is your place located ?' subtitle='Help guests find you!' />
-                <CountrySelect onChange={(value) => setCustomValue('location', value)} value={location} />
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Where is your place located ?"
+                    subtitle="Help guests find you!"
+                />
+                <CountrySelect
+                    onChange={(value) => setCustomValue('location', value)}
+                    value={location}
+                />
                 <Map center={location?.latlng} />
             </div>
-        )
+        );
     }
 
-    if(step === STEPS.INFO) {
+    if (step === STEPS.INFO) {
         bodyContent = (
-            <div className='flex flex-col gap-8'>
-                <Heading title='Share some basics about your place' subtitle='What amenities do you have ?' />
-                <Counter title='Guests' subtitle='How many guests do you allow ?' value={guestCount} onChange={(value) => setCustomValue('guestCount', value)} />
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Share some basics about your place"
+                    subtitle="What amenities do you have ?"
+                />
+                <Counter
+                    title="Guests"
+                    subtitle="How many guests do you allow ?"
+                    value={guestCount}
+                    onChange={(value) => setCustomValue('guestCount', value)}
+                />
                 <hr />
-                <Counter title='Rooms' subtitle='How many rooms do you have ?' value={roomCount} onChange={(value) => setCustomValue('roomCount', value)} />
+                <Counter
+                    title="Rooms"
+                    subtitle="How many rooms do you have ?"
+                    value={roomCount}
+                    onChange={(value) => setCustomValue('roomCount', value)}
+                />
                 <hr />
-                <Counter title='Bathrooms' subtitle='How many bathrooms do you have ?' value={bathroomCount} onChange={(value) => setCustomValue('bathroomCount', value)} />
+                <Counter
+                    title="Bathrooms"
+                    subtitle="How many bathrooms do you have ?"
+                    value={bathroomCount}
+                    onChange={(value) => setCustomValue('bathroomCount', value)}
+                />
             </div>
-        )
+        );
+    }
+
+    if (step === STEPS.IMAGES) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Add a photo of your place"
+                    subtitle="Show guests what your place looks like!"
+                />
+                <ImageUpload
+                    value={imageSrc}
+                    onChange={(value) => setCustomValue('imageSrc', value)}
+                />
+            </div>
+        );
     }
 
     const rentModal = useRentModal();
-  return (
-   <Modal title='Airbnb Your Home' body={bodyContent} isOpen={rentModal.isOpen} onClose={rentModal.onClose} onSubmit={onNext} actionLabel={actionLabel} secondaryActionLabel={secondaryActionLabel} secondaryAction={step === STEPS.CATEGORY ? undefined : onBack} />
-  )
-}
+    return (
+        <Modal
+            title="Airbnb Your Home"
+            body={bodyContent}
+            isOpen={rentModal.isOpen}
+            onClose={rentModal.onClose}
+            onSubmit={onNext}
+            actionLabel={actionLabel}
+            secondaryActionLabel={secondaryActionLabel}
+            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+        />
+    );
+};
 
-export default RentModal
+export default RentModal;
--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:[
+    images: {
+        domains: [
             'avatars.githubusercontent.com',
-            'lh3.googleusercontent.com'
-        ]
-    }
-}
+            'lh3.googleusercontent.com',
+            'res.cloudinary.com',
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
