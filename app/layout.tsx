6556c86a9d8fed4c22943d18306de4b799598440
Rafay Zia <rafayzia10@gmail.com>
2023-06-19 00:37:12 +0500

    Added listing feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,36 +4,36 @@ import ClientOnly from './components/ClientOnly';
 import LoginModal from './components/Modals/LoginModal';
 import RegisterModal from './components/Modals/RegisterModal';
 import RentModal from './components/Modals/RentModal';
-import Navbar from './components/navbar/Navbar'
-import './globals.css'
-import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar';
+import './globals.css';
+import { Nunito } from 'next/font/google';
 
-const font = Nunito({subsets:['latin']});
+const font = Nunito({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone By Rafay',
-}
+    title: 'Airbnb',
+    description: 'Airbnb Clone By Rafay',
+};
 
 export default async function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  const currentUser = await getCurrentUser();
+    const currentUser = await getCurrentUser();
 
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <ClientOnly>
-        <ToasterProvider/>
-        <RegisterModal/>
-        <LoginModal/>
-        <RentModal/>
-        <Navbar currentUser={currentUser} />
-        </ClientOnly>
-        {children}
-        </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <ClientOnly>
+                    <ToasterProvider />
+                    <RegisterModal />
+                    <LoginModal />
+                    <RentModal />
+                    <Navbar currentUser={currentUser} />
+                </ClientOnly>
+                <div className="pb-20 pt-28">{children}</div>
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,38 @@
+import { Listing } from '@prisma/client';
+import { getListings } from './actions/getListings';
+import ClientOnly from './components/ClientOnly';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
+import getCurrentUser from './actions/getCurrentUser';
 
-export default function Home() {
-  return (
-    <div className="text-rose-500">Hello airbnb</div>
-  )
+export default async function Home() {
+    const listings: Listing[] = await getListings();
+    const currentUser = await getCurrentUser();
+
+    if (listings && listings.length === 0) {
+        return (
+            <ClientOnly>
+                <EmptyState showReset />
+            </ClientOnly>
+        );
+    }
+
+    return (
+        <ClientOnly>
+            <Container>
+                <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+                    {listings.map((listing: Listing) => {
+                        return (
+                            <ListingCard
+                                key={listing.id}
+                                data={listing}
+                                currentUser={currentUser}
+                            />
+                        );
+                    })}
+                </div>
+            </Container>
+        </ClientOnly>
+    );
 }
