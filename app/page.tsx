f93a17f3b9451db5ca2f211bfa953940f107bd17
Rafay Zia <rafayzia10@gmail.com>
2023-06-26 18:56:49 +0500

    Fixed deployment issue

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,16 @@ import Container from './components/Container';
 import EmptyState from './components/EmptyState';
 import ListingCard from './components/listings/ListingCard';
 import getCurrentUser from './actions/getCurrentUser';
-import { SafeListing } from './types';
 
 interface HomeProps {
     searchParams: IListingParams;
 }
 
 const Home = async ({ searchParams }: HomeProps) => {
-    const listings: SafeListing[] = await getListings(searchParams);
+    const listings = await getListings(searchParams);
     const currentUser = await getCurrentUser();
 
-    if (listings && listings.length === 0) {
+    if (listings.length === 0) {
         return (
             <ClientOnly>
                 <EmptyState showReset />
@@ -26,7 +25,7 @@ const Home = async ({ searchParams }: HomeProps) => {
         <ClientOnly>
             <Container>
                 <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-                    {listings.map((listing: SafeListing) => {
+                    {listings.map((listing: any) => {
                         return (
                             <ListingCard
                                 key={listing.id}
